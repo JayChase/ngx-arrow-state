644c602983771e2bcf11eb96c3e8544cf03d0cd8
JayChase <JonathanChase@outlook.com>
2025-12-24 08:48:07 +0900

    🤞🏼

--- projects/ngx-arrow-state/src/lib/arrow-state/arrow-state.ts
+++ projects/ngx-arrow-state/src/lib/arrow-state/arrow-state.ts
@@ -1,18 +1,31 @@
-import { Directive, inject, OnInit } from '@angular/core';
+import { Directive, ElementRef, inject, input, OnInit } from '@angular/core';
 import { FormControlDirective, FormControlName, FormGroupDirective } from '@angular/forms';
 
 @Directive({
-  selector: '[ngxArrowState]',
+  selector: 'input[type="text"][ngxArrowState], textarea[ngxArrowState]',
   host: {
     '(keydown.arrowup)': 'onArrowUp($event)',
-    '(keydown.arrowdown)': 'onArrowUp($event)',
+    '(keydown.arrowdown)': 'onArrowDown($event)',
   },
 })
 export class ArrowState<T> implements OnInit {
+  private elementRef = inject<ElementRef<HTMLInputElement | HTMLTextAreaElement>>(ElementRef);
   private formGroupDirective = inject(FormGroupDirective, { optional: false });
   private formControlName = inject(FormControlName, { optional: true });
   private formControlDirective = inject(FormControlDirective, { optional: true });
   private formControl = this.formControlDirective || this.formControlName;
+
+  moveToStartOnUpArrow = input<boolean, boolean | null>(true, {
+    transform: (value: boolean | null) => (value ? value : false),
+  });
+
+  moveToEndOnDownArrow = input<boolean, boolean | null>(true, {
+    transform: (value: boolean | null) => (value ? value : false),
+  });
+
+  saveUnSubmittedValues = input<boolean, boolean | null>(true, {
+    transform: (value: boolean | null) => (value ? value : false),
+  });
 
   private history: T[] = [];
 
@@ -39,32 +52,60 @@ export class ArrowState<T> implements OnInit {
   }
 
   onArrowUp(event: Event): void {
-    event.preventDefault();
-    event.stopPropagation();
     //don't just pop as need to cycle through history with up and down arrows
     // also handle text area (up and down through them)
     if (this.formControl) {
-      const last = this.history.pop();
+      if (this.shouldChangeState('UP')) {
+        const last = this.history.pop();
 
-      if (last !== undefined) {
-        this.formControl.control.setValue(last);
-        this.history.unshift(last);
+        if (last !== undefined) {
+          this.formControl.control.setValue(last);
+          this.history.unshift(last);
+        }
       }
     }
   }
 
   onArrowDown(event: Event): void {
-    event.preventDefault();
-    event.stopPropagation();
     //don't just pop as need to cycle through history with up and down arrows
     // also handle text area (up and down through them)
     if (this.formControl) {
-      const first = this.history.shift();
+      if (this.shouldChangeState('DOWN')) {
+        const first = this.history.shift();
 
-      if (first !== undefined) {
-        this.formControl.control.setValue(first);
-        this.history.push(first);
+        if (first !== undefined) {
+          this.formControl.control.setValue(first);
+          this.history.push(first);
+        }
       }
+    }
+  }
+
+  private shouldChangeState(direction: 'UP' | 'DOWN'): boolean {
+    if (this.history.length < 2) {
+      return false;
+    }
+
+    if (this.elementRef.nativeElement.value.length === 0) {
+      return true;
+    }
+
+    // if the user is selecting text then do not change state
+    if (
+      this.elementRef.nativeElement.selectionStart !== this.elementRef.nativeElement.selectionEnd
+    ) {
+      return false;
+    }
+
+    //if the selectionEnd is the end length of the value
+    if (
+      (direction === 'UP' && this.elementRef.nativeElement.selectionStart === 0) ||
+      (direction === 'DOWN' &&
+        this.elementRef.nativeElement.selectionStart === this.elementRef.nativeElement.value.length)
+    ) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
